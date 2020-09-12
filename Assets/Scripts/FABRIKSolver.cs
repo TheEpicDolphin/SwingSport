d2e77e1892984e2b5482d9a6aa0092462c469dda
gdolphin <gdolphin@mit.edu>
2020-09-12 00:23:22 -0700

    making more progress on fabrik alg

--- Assets/Scripts/FABRIKSolver.cs
+++ Assets/Scripts/FABRIKSolver.cs
@@ -2,18 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class FreeJoint
+{
+    public Vector3 position;
+    public FreeJoint parent
+    {
+        get
+        {
+            return parent;
+        }
+        set
+        {
+            value.children.Add(this);
+            parent = value;
+        }
+    }
+    private List<FreeJoint> children;
+    public int childCount
+    {
+        get
+        {
+            return children.Count;
+        }
+    }
+
+    public FreeJoint()
+    {
+
+    }
+
+    public FreeJoint GetChild(int i)
+    {
+        return children[i];
+    }
+
+    
+}
+
 public class FABRIKSolver
 {
     const int maxIters = 25;
     const float tolerance = 1e-4f;
     private Transform root;
-    private Dictionary<Transform, float> jointLengthMap;
-    private Dictionary<Transform, Vector3> endEffectorToTargetMap;
-
+    private FreeJoint rootFreeJoint;
+    private Dictionary<FreeJoint, float> jointLengthMap;
+    private Dictionary<FreeJoint, Vector3> endEffectorToTargetMap;
+    
     public FABRIKSolver(Transform root, Transform[] endEffectors, Vector3[] targets)
     {
         this.root = root;
 
+        this.rootFreeJoint = new FreeJoint();
+        CreateFreeJointTree(root, rootFreeJoint);
+    }
+
+    private void CreateFreeJointTree(Transform trans, FreeJoint joint)
+    {
+        for (int i = 0; i < trans.childCount; i++)
+        {
+            Transform childTrans = trans.GetChild(i);
+            FreeJoint childJoint = new FreeJoint();
+            childJoint.parent = joint;
+            jointLengthMap[childJoint] = Vector3.Distance(childTrans.position, trans.position);
+            CreateFreeJointTree(childTrans, childJoint);
+        }
     }
 
     public void PlaceLimb()
@@ -100,21 +152,23 @@ public class FABRIKSolver
         int iters = 0;
         while (iters < maxIters)
         {
-            BackwardReach(root);
-            ForwardReach(root);
+            BackwardReach(rootFreeJoint);
+            ForwardReach(rootFreeJoint);
             iters += 1;
         }
         
     }
 
-    private Vector3 BackwardReach(Transform joint)
+    private Vector3 BackwardReach(FreeJoint joint)
     {
         if (endEffectorToTargetMap.ContainsKey(joint))
         {
             /* This is an end effector, set it to the corresponding target position */
             joint.position = endEffectorToTargetMap[joint];
+            ForwardReach(joint);
         }
-        else if(joint.childCount == 0)
+
+        if (joint.childCount == 0)
         {
             /* This chain has no end effectors in it. Don't change anything */
             return joint.parent.position;
@@ -124,7 +178,7 @@ public class FABRIKSolver
             Vector3 centroid = Vector3.zero;
             for (int i = 0; i < joint.childCount; i++)
             {
-                Transform child = joint.GetChild(i);
+                FreeJoint child = joint.GetChild(i);
                 centroid += BackwardReach(child);
             }
             /* Taking average of new positions calculated by this joint's children */
@@ -135,11 +189,11 @@ public class FABRIKSolver
         return newParentPos;
     }
 
-    private void ForwardReach(Transform joint)
+    private void ForwardReach(FreeJoint joint)
     {
         for (int i = 0; i < joint.childCount; i++)
         {
-            Transform child = joint.GetChild(i);
+            FreeJoint child = joint.GetChild(i);
             Vector3 v = child.position - joint.position;
             Vector3 newChildPos = joint.position + jointLengthMap[child] * v.normalized;
             child.position = newChildPos;
