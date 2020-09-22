4d0c4656a71cfd5ef77082d8f0de9c284cec9ac4
gdolphin <gdolphin@mit.edu>
2020-09-21 17:28:48 -0700

    it works, but i need to tweak it so that it feels useable in a game

--- Assets/Scripts/MagnetoGlove.cs
+++ Assets/Scripts/MagnetoGlove.cs
@@ -13,10 +13,10 @@ public class MagnetoGlove : MonoBehaviour
     FixedJoint ballHolder;
 
     float maxRange = 20.0f;
-    float magneticCoeff = 10.0f;
+    float magneticCoeff = 30.0f;
     /* This drag is performed against the ball's velocity component that 
      * is perpendicular to the direction from the glove to the ball*/
-    float magneticBallDrag = 5.0f;
+    float magneticBallDrag = 15.0f;
 
     bool equipped = true;
 
@@ -27,9 +27,6 @@ public class MagnetoGlove : MonoBehaviour
         ballTarget.parent = transform;
         ballTarget.position = transform.position + ballRadius * transform.forward;
         ballTarget.rotation = Quaternion.identity;
-
-        ballHolder = GetComponent<FixedJoint>();
-        ballHolder.breakForce = 200.0f;
     }
 
     // Update is called once per frame
@@ -40,43 +37,47 @@ public class MagnetoGlove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(equipped && PlayerInputManager.Instance.leftMouse && 
-            !ballHolder.connectedBody)
+        if (ballHolder && PlayerInputManager.Instance.spacebar)
+        {
+            ballHolder.connectedBody.AddForce(21.0f * Vector3.down);
+        }
+
+        // TODO: check if ball is in player's inventory.
+        if(equipped && PlayerInputManager.Instance.leftMouse && !ballHolder)
         {
             Collider[] colliders = new Collider[1];
             Physics.OverlapSphereNonAlloc(ballTarget.position, maxRange, colliders, 1 << 10);
             if (colliders[0])
             {
-                /* The ball only feels a force when within maxRange distance
-                 * of glove */
                 Rigidbody ballRb = colliders[0].GetComponent<Rigidbody>();
-                Vector3 r = ballRb.transform.position - ballTarget.position;
-                /* If ball is within 0.5f of glove, we do not increase
-                 * magnetic field any further*/
-                float rMag = Mathf.Max(r.magnitude, 0.5f);
-                Vector3 fMagnetic = r * magneticCoeff / (rMag * rMag);
-                /* This will help the ball go directly to the glove without oscillating
-                 * as much*/
-                Vector3 perpBallVel = Vector3.ProjectOnPlane(ballRb.velocity, r);
-                Vector3 ballDragForce = -magneticBallDrag * perpBallVel;
-                ballRb.AddForce(fMagnetic + ballDragForce);
-            }
-        }
-    }
+                Debug.Log(Vector3.Distance(ballTarget.position, ballRb.transform.position));
+                if (Vector3.Distance(ballTarget.position, ballRb.transform.position) < 0.1f)
+                {
+                    ballRb.isKinematic = true;
+                    ballRb.MovePosition(ballTarget.position);
+                    ballRb.isKinematic = false;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        /* Handle when ball is close enough to glove to be grabbed */
-        if (equipped && PlayerInputManager.Instance.leftMouse && 
-            other.tag == "ball")
-        {
-            Rigidbody ballRb = other.GetComponent<Rigidbody>();
-            if (ballRb)
-            {
-                ballRb.isKinematic = true;
-                ballRb.MovePosition(ballTarget.position);
-                ballRb.isKinematic = false;
-                ballHolder.connectedBody = ballRb;
+                    ballHolder = gameObject.AddComponent<FixedJoint>();
+                    ballHolder.connectedBody = ballRb;
+                    ballHolder.breakForce = 20.0f;
+                }
+                else
+                {
+                    /* The ball only feels a force when within maxRange distance
+                    * of glove */
+                    Vector3 r = ballRb.transform.position - ballTarget.position;
+                    /* If ball is within 0.5f of glove, we do not increase
+                     * magnetic field any further*/
+                    float rMag = Mathf.Max(r.magnitude, 0.5f);
+                    Vector3 fMagnetic = r * magneticCoeff / (rMag * rMag);
+                    /* This will help the ball go directly to the glove without oscillating
+                     * as much*/
+                    Vector3 perpBallVel = Vector3.ProjectOnPlane(ballRb.velocity, r);
+                    Vector3 ballDragForce = -magneticBallDrag * perpBallVel;
+                    ballRb.AddForce(-fMagnetic + ballDragForce);
+                }
+
+                
             }
         }
     }
