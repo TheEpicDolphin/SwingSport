bd88db25e1d680c4c3f470ea2f92624686605792
gdolphin <gdolphin@mit.edu>
2020-11-07 16:05:13 -0800

    gameobjectstate

---
+++ Assets/Scripts/StateMachine/GameObjectState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameObjectState
+{
+    protected GameObject gameObject;
+
+    protected GameObjectStateMachine gameObjectSM;
+
+    public GameObjectState(GameObjectStateMachine gameObjectSM, GameObject gameObject)
+    {
+        this.gameObjectSM = gameObjectSM;
+        this.gameObject = gameObject;
+    }
+
+    public abstract void OnEnter();
+
+    public abstract void UpdateStep();
+
+    public abstract void FixedUpdateStep();
+
+    public abstract void OnExit();
+}
---
+++ Assets/Scripts/StateMachine/GameObjectStateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class GameObjectStateMachine
+{
+    GameObjectState currentState;
+    private Dictionary<Type, GameObjectState> stateMap = new Dictionary<Type, GameObjectState>();
+
+    public GameObjectStateMachine(object context, List<Type> stateTypes)
+    {
+        foreach (Type stateType in stateTypes)
+        {
+            stateMap[stateType] = (GameObjectState)Activator.CreateInstance(stateType, new object[] { this, context });
+        }
+    }
+
+    public void InitWithState<T>() where T : GameObjectState
+    {
+        currentState = GetState<T>();
+        currentState.OnEnter();
+    }
+
+    private GameObjectState GetState<T>() where T : GameObjectState
+    {
+        return stateMap[typeof(T)];
+    }
+
+    public void TransitionToState<T>() where T : GameObjectState
+    {
+        currentState.OnExit();
+        currentState = GetState<T>();
+        currentState.OnEnter();
+    }
+
+    public void UpdateStep()
+    {
+        currentState.UpdateStep();
+    }
+
+    public void FixedUpdateStep()
+    {
+        currentState.FixedUpdateStep();
+    }
+}
